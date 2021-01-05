cef7de8d142ce031079fbcdcdf48064ed241c994
mosmolov <57736073+mosmolov@users.noreply.github.com>
2021-01-05 17:27:27 -0500

    update index.js
    took out config
    added monitoring capability
    added webhook/embed

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 var request = require('request');
 const discord = require('discord.js');
 const client = new discord.Client();
-const { bottoken, ProductSKUs, BBYAPIKEY } = require('./config.json');
+const { ProductSKUs, BBYAPIKEY } = require('./config.json');
 
-var itemconfig = {
-    "Product Name": "",
-    "SKU": "",
-    "Availability": "",
-    "Price": "",
-    "Time of Restock": "",
-    "ATC Link": "",
-    "Product Image": ""
-}
-var items = [];
 function sleep() {
     return new Promise((resolve) => {
       setTimeout(resolve, 1000);
@@ -29,14 +19,25 @@ ProductSKUs.forEach(sku => {
     request(options, (err, res) => {
         if(err) ;
         var itemdata = JSON.parse(res.body);
-        itemconfig["Product Name"] = itemdata["name"];
-        itemconfig["SKU"] = sku;
-        itemconfig["Availability"] = itemdata["orderable"];
-        itemconfig["Time of Restock"] = itemdata["itemUpdateDate"];
-        itemconfig["Price"] = itemdata["regularPrice"];
-        itemconfig["Product Image"] = itemdata["image"];
-        itemconfig["ATC Link"] = itemdata["addToCartUrl"];
-        console.log(itemconfig);
+        if(itemdata["orderable"] != 'SoldOut' || itemdata["orderable"] != undefined){
+            const embed = new discord.MessageEmbed()
+                            .setThumbnail(itemdata["image"])
+                            .setFooter(`Restocked at ${itemdata["itemUpdateDate"]}`)
+                            .setTitle(`${itemdata["name"]}`)
+                            .setTimestamp()
+                            .addField('SKU', sku, true)
+                            .addField('Price',itemdata["regularPrice"], true)
+                            .addField('Important Links',`**[ATC](${itemdata["addToCartUrl"]})** - **[Product](${itemdata['url']})**`);
+            //https://discord.com/api/webhooks/773211136706609232/qZfnFkoxQSX_zYxADC5SdukN1vACMXYyAYFqJoeABVT7ThqPfbqUWLH_FdDQqBYi7wFi
+            const hook = new discord.WebhookClient('773211136706609232', 'qZfnFkoxQSX_zYxADC5SdukN1vACMXYyAYFqJoeABVT7ThqPfbqUWLH_FdDQqBYi7wFi')
+            hook.send(embed);                
+        }
+        /*itemdata["name"];
+        itemdata["orderable"];
+        itemdata["itemUpdateDate"];
+        itemdata["regularPrice"];
+        itemdata["image"];
+        itemdata["addToCartUrl"];*/
     })
     delay();
 });
