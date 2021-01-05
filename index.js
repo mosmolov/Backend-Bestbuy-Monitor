84e6b18e693062b94c95508465be4e0fe52c2048
mosmolov <57736073+mosmolov@users.noreply.github.com>
2021-01-05 15:12:20 -0500

    update index
    added scraping

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
 var request = require('request');
-
+const discord = require('discord.js');
+const client = new discord.Client();
 const { bottoken, ProductSKUs, BBYAPIKEY } = require('./config.json');
-var options = {
-    'method': 'GET',
-    'url': `https://api.bestbuy.com/v1/products/${sku1}.json?apiKey=${apikey}&format=json-pretty`,
+
+var itemconfig = {
+    "Product Name": "",
+    "SKU": "",
+    "Availability": "",
+    "Price": "",
+    "Time of Restock": "",
+    "ATC Link": "",
+    "Product Image": ""
 }
-request(options, (err, res) => {
-    let productinfo = JSON.parse(res.body); 
-    console.dir(productinfo)
-})
-  
+var items = [];
+function sleep() {
+    return new Promise((resolve) => {
+      setTimeout(resolve, 1000);
+    });
+  }
+async function delay() {
+    await sleep(3000);
+}
+ProductSKUs.forEach(sku => {
+    var options = {
+        'method': 'GET',
+        'url': `https://api.bestbuy.com/v1/products/${sku}.json?apiKey=${BBYAPIKEY}&format=json`
+    }
+    request(options, (err, res) => {
+        if(err) ;
+        var itemdata = JSON.parse(res.body);
+        itemconfig["Product Name"] = itemdata["name"];
+        itemconfig["SKU"] = sku;
+        itemconfig["Availability"] = itemdata["orderable"];
+        itemconfig["Time of Restock"] = itemdata["itemUpdateDate"];
+        itemconfig["Price"] = itemdata["regularPrice"];
+        itemconfig["Product Image"] = itemdata["image"];
+        itemconfig["ATC Link"] = itemdata["addToCartUrl"];
+        console.log(itemconfig);
+    })
+    delay();
+});
+
+
+
+
+
+
